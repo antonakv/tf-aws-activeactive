a3e243d33546319f39260d20af03f3511045cf61
Anton Akulov <antonakv@users.noreply.github.com>
2022-08-08 11:36:37 +0200

    Policies for AWS

---
+++ main.tf
@@ -0,0 +1,89 @@
+locals {
+  friendly_name_prefix = random_string.friendly_name.id
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
+data "aws_iam_policy_document" "secretsmanager" {
+  statement {
+    actions   = ["secretsmanager:GetSecretValue"]
+    effect    = "Allow"
+    resources = [aws_secretsmanager_secret_version.tfe_license.secret_id]
+    sid       = "AllowSecretsManagerSecretAccess"
+  }
+}
+
+data "aws_iam_policy_document" "tfe_asg_discovery" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:Describe*"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "random_string" "friendly_name" {
+  length  = 4
+  upper   = false
+  numeric = false
+  special = false
+}
+
+data "aws_iam_policy_document" "instance_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "instance_role" {
+  name_prefix        = "${var.friendly_name_prefix}-tfe"
+  assume_role_policy = data.aws_iam_policy_document.instance_role.json
+}
+
+resource "aws_iam_instance_profile" "tfe" {
+  name_prefix = "${var.friendly_name_prefix}-tfe"
+  role        = aws_iam_role.instance_role.name
+}
+
+resource "aws_secretsmanager_secret" "tfe_license" {
+  description = "The TFE license"
+}
+
+resource "aws_secretsmanager_secret_version" "tfe_license" {
+  secret_binary = filebase64(var.tfe_license_path)
+  secret_id     = aws_secretsmanager_secret.tfe_license.id
+}
+
+resource "aws_iam_role_policy" "tfe_asg_discovery" {
+  name   = "${var.friendly_name_prefix}-tfe-asg-discovery"
+  role   = aws_iam_role.instance_role.id
+  policy = data.aws_iam_policy_document.tfe_asg_discovery.json
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "friendly_name_prefix" {
+  type        = string
+  description = "Prefix for names"
+}
+
+variable "tfe_license_path" {
+  type        = string
+  description = "Path for the TFE license"
+}
+
